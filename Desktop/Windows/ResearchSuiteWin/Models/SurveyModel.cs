495efd1e39f78ed1f1b21690cece2465b3ee6525
ThanHtikeAung <aung@aung.me>
2017-12-04 18:03:17 +0630

    add baseStep and SurveyModel

---
+++ Desktop/Windows/ResearchSuiteWin/Models/SurveyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchSuiteWin.Models
+{
+    class SurveyModel
+    {
+        public string surveyId { get; set; }
+        public string version { get; set; }
+        public uint build { get; set; }
+        public DateTime releaseDt { get; set; }
+        public string termsAndConditions { get; set; }
+        public bool isRequireSignature { get; set; }
+        public bool isRequireToAccept { get; set; }
+    }
+}
