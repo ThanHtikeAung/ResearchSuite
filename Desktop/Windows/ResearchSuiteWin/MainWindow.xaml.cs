647bf2145c68d99b8f0546a32830e7a84131570c
ThanHtikeAung <aung@aung.me>
2017-12-05 18:09:09 +0630

    write output json to zip file.

--- Desktop/Windows/ResearchSuiteWin/MainWindow.xaml.cs
+++ Desktop/Windows/ResearchSuiteWin/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Newtonsoft.Json;
 using ResearchSuiteWin.Models;
-using Newtonsoft;
-using Newtonsoft.Json;
+using System.IO;
+using System.IO.Compression;
+using System.Windows;
 
 namespace ResearchSuiteWin
 {
@@ -33,6 +21,18 @@ namespace ResearchSuiteWin
         private void btnCreateSurvey_Click(object sender, RoutedEventArgs e)
         {
             string json = JsonConvert.SerializeObject(this.model);
+
+            using (FileStream zipToOpen = new FileStream("release.zip", FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                {
+                    ZipArchiveEntry readmeEntry = archive.CreateEntry("data.txt");
+                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+                    {
+                        writer.WriteLine(json);
+                    }
+                }
+            }
             System.Diagnostics.Debug.WriteLine(json);
         }
 
